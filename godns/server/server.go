7b61609b54d848b1b7ff26e789949ce520962cfa
tooBusyNow <OldsmobileBuick54@yandex.ru>
2022-05-13 03:54:26 +0500

    added support for A type

--- godns/server/server.go
+++ godns/server/server.go
@@ -31,91 +31,86 @@ func StartServer(handler *ConfigHandler, mainContext context.Context, cache *Cac
 	}
 
 	fmt.Print("\033[32mDNS Server is up and running\n\033[0m")
-	serveRequest(handler, mainContext, conn, cache)
+	go serveRequest(handler, mainContext, conn, cache)
 }
 
-func serveRequest(handler *ConfigHandler, mainContext context.Context, conn *net.UDPConn, cache *Cache) {
+func serveRequest(handler *ConfigHandler, mainContext context.Context, intConn *net.UDPConn, cache *Cache) {
 	var buffer [1024]byte
 	for {
 		select {
 		case <-mainContext.Done():
-			conn.Close()
+			intConn.Close()
 			return
 
 		default:
 			if handler.NeedRestart {
-				conn.Close()
+				intConn.Close()
 				return
 			}
-			conn.SetReadDeadline(time.Now().Add(time.Second))
-			_, addr, _ := conn.ReadFromUDP(buffer[:])
-
+			intConn.SetReadDeadline(time.Now().Add(time.Second * 3))
+			n, addr, _ := intConn.ReadFromUDP(buffer[:])
 			if addr != nil {
-				rawPacket := gopacket.NewPacket(buffer[:], layers.LayerTypeDNS, gopacket.Default)
+				rawPacket := gopacket.NewPacket(buffer[:n], layers.LayerTypeDNS, gopacket.Default)
 				dnsPacket := rawPacket.Layer(layers.LayerTypeDNS).(*layers.DNS)
 
 				if dnsPacket != nil {
-					go serveDNSPacket(handler, handler.Get().Nameserver, dnsPacket, cache)
+					go serveDNSPacket(handler, handler.Get().Nameserver, dnsPacket, cache, intConn)
 				}
 			}
 		}
 	}
 }
 
-func serveDNSPacket(handler *ConfigHandler, serverIP string, dnsPacket *layers.DNS, cache *Cache) error {
+func serveDNSPacket(handler *ConfigHandler, serverIP string,
+	dnsPacket *layers.DNS, cache *Cache, intConn *net.UDPConn) error {
 
 	var config *ConfigInstance = handler.Get()
 	var someErr error
 
 	udpExternal := serverIP + ":" + strconv.Itoa(config.ExternalPort)
-	conn, err := net.Dial("udp", udpExternal)
+	extConn, err := net.Dial("udp", udpExternal)
 	if err != nil {
 		fmt.Println("\033[31mUnable to establish connection to External DNS server\033[0m", udpExternal)
 		os.Exit(0)
 	}
 
-	fmt.Println(serverIP)
-
 	for _, quest := range dnsPacket.Questions {
 		if quest.Type.String() == "A" {
 			p := make([]byte, 2048)
-			fmt.Fprintf(conn, string(dnsPacket.Contents))
+			fmt.Fprintf(extConn, string(dnsPacket.Contents))
 
-			conn.SetReadDeadline(time.Now().Add(time.Second * 3))
-			_, err = bufio.NewReader(conn).Read(p)
+			extConn.SetReadDeadline(time.Now().Add(time.Second * 3))
+			n, err := bufio.NewReader(extConn).Read(p)
 
 			if err != nil {
 				return errors.New("Timeout")
 			}
-			conn.Close()
-
-			rawPacket := gopacket.NewPacket(p[:], layers.LayerTypeDNS, gopacket.Default)
+			extConn.Close()
+			rawPacket := gopacket.NewPacket(p[:n], layers.LayerTypeDNS, gopacket.Default)
 			dnsResponse := rawPacket.Layer(layers.LayerTypeDNS).(*layers.DNS)
-
-			fmt.Println(dnsResponse.ResponseCode)
 
 			if len(dnsResponse.Answers) > 0 {
 				fmt.Println("Finished")
 				fmt.Println(dnsResponse.Answers[0].IP)
+				var udpAddr = &net.UDPAddr{
+					IP:   net.ParseIP(config.Host),
+					Port: config.InternalPort,
+				}
+				intConn.WriteToUDP(dnsResponse.Contents, udpAddr)
+
 				return nil
-			} else if dnsResponse.ResponseCode.String() == "Format Error" {
-				return errors.New("FE")
+
 			} else {
 				for _, nsIP := range dnsResponse.Additionals {
-					fmt.Println("Got nameserver: ", nsIP.IP.String())
-
 					if nsIP.IP.To4() == nil {
 						continue
 					}
-
-					someErr = serveDNSPacket(handler, nsIP.IP.String(), dnsPacket, cache)
-					fmt.Println(someErr)
+					someErr = serveDNSPacket(handler, nsIP.IP.String(), dnsPacket, cache, intConn)
 					if someErr == nil {
 						break
 					}
 				}
 			}
-
 		}
 	}
 	return someErr
